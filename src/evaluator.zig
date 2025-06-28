b96a0f8d92d7d33ec2b337bae60c251bbf358ad2
Hamed Mohamed <rite2hamed@gmail.com>
2025-06-28 12:27:37 -0700

    #oq9

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -3,6 +3,9 @@ pub const Evaluator = @This();
 pub fn evaluate(expr: *Expr) !LiteralExpr {
     switch (expr.*) {
         .literal => |lit| return lit,
+        .grouping => |grp| {
+            return try evaluate(grp);
+        },
         else => {
             return error.NotImplemented;
         },
